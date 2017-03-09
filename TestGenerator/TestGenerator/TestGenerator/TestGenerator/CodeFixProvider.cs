3a649ce9aefc002561c2e44a4c30eff64d88b9d7
Tuba Kaya <tuba@funda.nl>
2017-03-09 18:53:41 +0100

    Added TestGenerator

---
+++ TestGenerator/TestGenerator/TestGenerator/TestGenerator/CodeFixProvider.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+
+namespace TestGenerator
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(TestGeneratorCodeFixProvider)), Shared]
+    public class TestGeneratorCodeFixProvider : CodeFixProvider
+    {
+        private const string title = "(Re-)Generate SetUp";
+
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(TestGeneratorAnalyzer.DiagnosticId); }
+        }
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            // Find the type declaration identified by the diagnostic.
+            var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<ClassDeclarationSyntax>().First();
+
+            if (declaration.Identifier.Text.EndsWith("Tests"))
+            {
+                // Register a code action that will invoke the fix.
+                context.RegisterCodeFix(
+                    CodeAction.Create(
+                        title: title,
+                        createChangedDocument: c => CreateSetupMethod(context.Document, declaration, c),
+                        equivalenceKey: title),
+                    diagnostic);
+            }
+        }
+
+        private async Task<Document> CreateSetupMethod(Document document, ClassDeclarationSyntax classDecl, CancellationToken cancellationToken)
+        {
+            var generator = SyntaxGenerator.GetGenerator(document);
+            var setupMethodDeclaration = SetupMethod(generator);
+
+            var root = await document.GetSyntaxRootAsync(cancellationToken);
+            var existingSetupMethod =root.DescendantNodes().OfType<MethodDeclarationSyntax>()
+                                                            .FirstOrDefault(_ => _.Identifier.Text == "Setup");
+
+            SyntaxNode newRoot;
+            if (existingSetupMethod != null)
+            {
+                newRoot = root.ReplaceNode(existingSetupMethod, setupMethodDeclaration);
+            }
+            else
+            {
+                var newClassDecl= classDecl.AddMembers(setupMethodDeclaration as MemberDeclarationSyntax);
+                newRoot = root.ReplaceNode(classDecl, newClassDecl);
+            }
+
+            var newDocument = document.WithSyntaxRoot(newRoot);
+            return newDocument;
+        }
+
+        private SyntaxNode SetupMethod(SyntaxGenerator generator)
+        {
+            var setupMethodDeclaration = generator.MethodDeclaration("Setup", null,
+            null, null,
+            Accessibility.Public,
+            DeclarationModifiers.None,
+            null);
+            var setupAttribute = generator.Attribute("SetUp");
+
+            var setupMethodWithSetupAttribute = generator.InsertAttributes(setupMethodDeclaration, 0, setupAttribute);
+            return setupMethodWithSetupAttribute;
+        }
+    }
+}
---
+++ TestGenerator/TestGenerator/TestGenerator/TestGenerator/NUnitTestSetupGenerator.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+
+namespace TestGenerator
+{
+    public interface INUnitTestSetupGenerator
+    {
+        Task<SyntaxNode> TestClassRootAsync(Document document, string className);
+    }
+
+    public class NUnitTestSetupGenerator : INUnitTestSetupGenerator
+    {
+        public async Task<SyntaxNode> TestClassRootAsync(Document document, string className)
+        {
+            var docSyntaxRoot = await document.GetSyntaxRootAsync();
+            var semanticModel = await document.GetSemanticModelAsync();
+            var classDec = docSyntaxRoot.DescendantNodes().OfType<ClassDeclarationSyntax>().Where(x => x.Identifier.Text == className).First();
+
+            var generator = SyntaxGenerator.GetGenerator(document);
+            List<UsingDirectiveSyntax> usingDirectives = UsingDirectives(docSyntaxRoot, generator);
+            IEnumerable<SyntaxNode> fieldDeclarations = FieldDeclarations(className, classDec, generator);
+            var setupMethodDeclaration = SetupMethod(className, classDec, generator);
+
+            var members = new List<SyntaxNode>();
+            members.AddRange(fieldDeclarations);
+            members.Add(setupMethodDeclaration);
+
+            SyntaxNode classWithTestFixtureAttribute = ClassDeclaration(classDec, generator, members);
+            SyntaxNode namespaceDeclaration = NamespaceDeclaration(docSyntaxRoot, generator, classWithTestFixtureAttribute);
+
+            var allNodes = new List<SyntaxNode>();
+            allNodes.AddRange(usingDirectives);
+            allNodes.Add(namespaceDeclaration);
+
+            // Get a CompilationUnit (code file) for the generated code
+            var newNode = generator.CompilationUnit(allNodes).NormalizeWhitespace();
+
+            return newNode;
+        }
+
+        private SyntaxNode NamespaceDeclaration(SyntaxNode docSyntaxRoot, SyntaxGenerator generator, SyntaxNode classWithTestFixtureAttribute)
+        {
+            var namespaceDeclarationSyntax = docSyntaxRoot.DescendantNodes().OfType<NamespaceDeclarationSyntax>().FirstOrDefault();
+            string namespaceText = namespaceDeclarationSyntax != null ? namespaceDeclarationSyntax.Name.ToString() : "MyTests";
+            var namespaceDeclaration = generator.NamespaceDeclaration(namespaceText, classWithTestFixtureAttribute);
+            return namespaceDeclaration;
+        }
+
+        private SyntaxNode ClassDeclaration(ClassDeclarationSyntax classDec, SyntaxGenerator generator, List<SyntaxNode> members)
+        {
+            var classDefinition = generator.ClassDeclaration(
+                         classDec.Identifier.Text + "Tests",
+                         typeParameters: null,
+                         accessibility: Accessibility.Public,
+                         modifiers: DeclarationModifiers.None,
+                         baseType: null,
+                         interfaceTypes: null,
+                         members: members);
+
+            var testFixtureAttribute = generator.Attribute("TestFixture");
+
+            var classWithTestFixtureAttribute = generator.InsertAttributes(classDefinition, 0, testFixtureAttribute);
+            return classWithTestFixtureAttribute;
+        }
+
+        private IEnumerable<SyntaxNode> FieldDeclarations(string className, ClassDeclarationSyntax classDec, SyntaxGenerator generator)
+        {
+            var fieldDeclarations = new List<SyntaxNode>();
+
+            var constructorWithParameters =
+                classDec.DescendantNodes()
+                    .OfType<ConstructorDeclarationSyntax>()
+                    .FirstOrDefault(
+                        x => x.ParameterList.Parameters.Any());
+            if (constructorWithParameters != null)
+            {
+                var constructorParam = constructorWithParameters.ParameterList.Parameters;
+                foreach (var parameter in constructorParam)
+                {
+                    var parameterType = parameter.Type;
+                    var fieldName = string.Format("_{0}", parameterType.ToString().ToLowerInvariant());
+                    var fieldDec = generator.FieldDeclaration(fieldName
+                                                            , parameterType
+                                                            , Accessibility.Private);
+                    fieldDeclarations.Add(fieldDec);
+
+                }
+            }
+            return fieldDeclarations;
+        }
+
+        private SyntaxNode SetupMethod(string className, ClassDeclarationSyntax classDec, SyntaxGenerator generator)
+        {
+            var fieldDeclarations = new List<SyntaxNode>();
+            var expressionStatements = new List<SyntaxNode>();
+            var constructorWithParameters =
+                classDec.DescendantNodes()
+                    .OfType<ConstructorDeclarationSyntax>()
+                    .FirstOrDefault(
+                        x => x.ParameterList.Parameters.Any());
+            if (constructorWithParameters != null)
+            {
+                var constructorParam = constructorWithParameters.ParameterList.Parameters;
+                foreach (var parameter in constructorParam)
+                {
+                    var parameterType = parameter.Type;
+                    var fieldName = string.Format("_{0}", parameterType.ToString().ToLowerInvariant());
+                    var fieldDec = generator.FieldDeclaration(fieldName
+                                                            , parameterType
+                                                            , Accessibility.Private);
+                    fieldDeclarations.Add(fieldDec);
+
+                    var fieldIdentifier = generator.IdentifierName(fieldName);
+                    var mocksRepositoryIdentifier = generator.IdentifierName("MocksRepository");
+                    var parameterTypeIdentifier = generator.IdentifierName(parameterType.ToString());
+
+                    var memberAccessExpression = generator.MemberAccessExpression(mocksRepositoryIdentifier, generator.GenericName("GenerateStub", parameterTypeIdentifier));
+                    var invocationExpression = generator.InvocationExpression(memberAccessExpression);
+
+                    var expressionStatementSettingField = generator.AssignmentStatement(fieldIdentifier, invocationExpression);
+                    expressionStatements.Add(expressionStatementSettingField);
+                }
+            }
+
+            var constructorParameters = fieldDeclarations.SelectMany(x => x.DescendantNodes().OfType<VariableDeclaratorSyntax>().Select(y => y.Identifier.Text));
+
+            var setupBody = new List<SyntaxNode>();
+            setupBody.AddRange(expressionStatements);
+
+            var targetObjectCreationExpression = generator.ObjectCreationExpression(generator.IdentifierName(className), constructorParameters.Select(x => generator.IdentifierName(x)));
+
+            var expressionStatementTargetInstantiation = generator.AssignmentStatement(generator.IdentifierName("_target"), targetObjectCreationExpression);
+            setupBody.Add(expressionStatementTargetInstantiation);
+
+            // Generate the Clone method declaration
+            var setupMethodDeclaration = generator.MethodDeclaration("Setup", null,
+              null, null,
+              Accessibility.Public,
+              DeclarationModifiers.None,
+              setupBody);
+            var setupAttribute = generator.Attribute("SetUp");
+
+            var setupMethodWithSetupAttribute = generator.InsertAttributes(setupMethodDeclaration, 0, setupAttribute);
+            return setupMethodWithSetupAttribute;
+        }
+
+        private static List<UsingDirectiveSyntax> UsingDirectives(SyntaxNode docSyntaxRoot, SyntaxGenerator generator)
+        {
+            var usingDirectives = docSyntaxRoot.ChildNodes().OfType<UsingDirectiveSyntax>().ToList();
+            usingDirectives.Add(generator.NamespaceImportDeclaration("Rhino.Mocks") as UsingDirectiveSyntax);
+            return usingDirectives;
+        }
+    }
+}
